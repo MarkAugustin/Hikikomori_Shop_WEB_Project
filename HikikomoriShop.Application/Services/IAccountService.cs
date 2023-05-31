1826c22a407dc73d3ee94553b9ec2207236b9951
Mark Stepanenco <marc-augustin.stepanenco@isa.utm.md>
2023-05-31 18:50:20 +0300

    Application Services

---
+++ HikikomoriShop.Application/Services/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using HikikomoriShop.Domain;
+
+namespace HikikomoriShop.Application.Services;
+
+public interface IAccountService
+{
+    Task SaveAsync();
+    void InsertAccount (User user);
+    void InsertRole (Role role);
+    Task<Role> GetRoleByNameAsync(string name);
+    Task<User> GetByEmailAsync(string email);
+    Task<User> GetByIdAsync(string id);
+    Task<User> GetByIdWithoutTrackingAsync(string id);
+    Task<User> LoginAsync(string email, string password);
+    Task<List<User>> GetAllAsync();
+    void Update(User user);
+    void Delete(User user);
+}
---
+++ HikikomoriShop.Application/Services/ProductService.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HikikomoriShop.Domain.Entities;
+using HikikomoriShop.Infrastructure;
+
+namespace HikikomoriShop.Application.Services;
+
+public interface IProductService
+{
+    Task SaveAsync();
+    void InsertProduct(Product product);
+    void Update(Product product);
+    void Delete(Product product);
+    Task<Product> GetByIdAsync(string id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<IEnumerable<Product>> GetAllWithCategoryAsync();
+    Task<IEnumerable<Product>> GetByCategoryAsync(string categoryName);
+    Task<Product> GetByNameAsync(string name);
+    Task<bool> IsExistByIdAsync(string id);
+    Task<bool> IsExistByNameAsync(string name);
+    void InsertCategory(Category category);
+    Task<Category> GetCategoryByNameAsync(string name);
+}
+
+public class ProductService : IProductService
+{
+    private readonly ApplicationDbContext _context;
+    
+    public ProductService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Task SaveAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
+    
+    public void InsertProduct(Product product)
+    {
+        _context.Products.Add(product);
+    }
+    
+    public void Update(Product product)
+    {
+        _context.Products.Update(product);
+    }
+    
+    public void Delete(Product product)
+    {
+        _context.Products.Remove(product);
+    }
+    
+    public async Task<Product> GetByIdAsync(string id)
+    {
+        return await _context.Products.FirstOrDefaultAsync(p => p != null && p.Id == id);
+    }
+    
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+    
+    public async Task<IEnumerable<Product>> GetAllWithCategoryAsync()
+    {
+        return await _context.Products.Include(p => p.Category).ToListAsync();
+    }
+    
+    public async Task<IEnumerable<Product>> GetByCategoryAsync(string categoryName)
+    {
+        return await _context.Products.Where(p => p.Category.Name == categoryName).ToListAsync();
+    }
+    
+    public async Task<Product> GetByNameAsync(string name)
+    {
+        return await _context.Products.FirstOrDefaultAsync(p => p.Name == name);
+    }
+
+    public async Task<bool> IsExistByIdAsync(string id)
+    {
+        return await _context.Products.AnyAsync(p => p != null && p.Id == id);
+    }
+    
+    public async Task<bool> IsExistByNameAsync(string name)
+    {
+        return await _context.Products.AnyAsync(p => p != null && p.Name == name);
+    }
+    
+    public void InsertCategory(Category category)
+    {
+        _context.Categories.Add(category);
+    }
+    
+    public async Task<Category> GetCategoryByNameAsync(string name)
+    {
+        return await _context.Categories.FirstOrDefaultAsync(c => c != null && c.Name == name);
+    }
+}
