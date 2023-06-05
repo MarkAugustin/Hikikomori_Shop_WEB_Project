ae946202b908b33640383b2093d3d96806fd3e18
PavelRogalin <pavel.rogalin@isa.utm.md>
2023-06-05 20:47:12 +0300

    Pages

---
+++ HikikomoriShop.UI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HikikomoriShop.UI.Controllers;
+
+public class CategoryController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
