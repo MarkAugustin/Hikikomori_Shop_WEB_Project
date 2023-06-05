3996a2caf1766de42c567ab8ee3554be41da7d81
PavelRogalin <pavel.rogalin@isa.utm.md>
2023-06-05 19:55:47 +0300

    DataBaseControllers

---
+++ HikikomoriShop.UI/Controllers/AccountController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HikikomoriShop.Application.Services;
+using HikikomoriShop.Domain;
+
+namespace HikikomoriShop.UI.Controllers;
+
+[Controller]
+[Route("[controller]/[action]")]
+public class AccountController : Controller
+{
+    private readonly IAccountService _accountService;
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Index()
+    {
+        var users = await _accountService.GetAllAsync();
+        return View(users);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+public async Task<IActionResult> Delete(string id)
+    {
+        var user = await _accountService.GetByIdAsync(id);
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    {
+        var user = await _accountService.GetByIdAsync(id);
+        _accountService.Delete(user);
+        await _accountService.SaveAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    public async Task<IActionResult> Create(User user)
+    {
+        return View(user);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [ActionName("Create")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateConfirmed(User user)
+    {
+        var userFromDb = await _accountService.GetByEmailAsync(user.Email);
+        if (userFromDb is not null)
+        {
+            return RedirectToAction("Login", "AccountSession");
+        }
+        
+        var roleFromDb = await _accountService.GetRoleByNameAsync(user.Role?.Name);
+        if (roleFromDb is null)
+        {
+            roleFromDb = new Role
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = user.Role?.Name
+            };
+            _accountService.InsertRole(roleFromDb);
+        }
+            
+        userFromDb = new User
+        {
+            Id = Guid.NewGuid().ToString(), 
+            Email = user.Email, 
+            Password = user.Password,
+            Role = roleFromDb
+        };
+            
+        _accountService.InsertAccount(userFromDb);   
+        await _accountService.SaveAsync();
+        return RedirectToAction(nameof(Index));
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    public async Task<IActionResult> Update(string id)
+    {
+        var user = await _accountService.GetByIdAsync(id);
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+    
+    [ActionName("Update")]
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateConfirmed(User user)
+    {
+        var roleFromDb = await _accountService.GetRoleByNameAsync(user.Role?.Name);
+        if (roleFromDb is not null)
+        {
+            user.Role = roleFromDb; 
+        }
+        else
+        {
+            user.Role = await _accountService.GetRoleByNameAsync("User");
+        }
+        
+        _accountService.Update(user);
+        await _accountService.SaveAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ HikikomoriShop.UI/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HikikomoriShop.Application.Services;
+using HikikomoriShop.Domain.Entities;
+
+namespace HikikomoriShop.UI.Controllers;
+
+[Controller]
+[Route("[controller]/[action]")]
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+    
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productService.GetAllWithCategoryAsync();
+        return View(products);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+public async Task<IActionResult> Delete(string id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    {
+        var productFromDb = await _productService.GetByIdAsync(id);
+        _productService.Delete(productFromDb);
+        await _productService.SaveAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    public async Task<IActionResult> Create(Product product)
+    {
+        return View(product);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [ActionName("Create")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateConfirmed(Product product)
+    {
+        product.Id = Guid.NewGuid().ToString();
+        product.Category = await _productService.GetCategoryByNameAsync("Default");
+        _productService.InsertProduct(product);   
+        await _productService.SaveAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
