fcd5eb0ba790bb8cbea105cb462fe02e97af4fe8
Vo01007 <dimon.v.18@gmail.com>
2023-06-05 11:41:32 +0300

    UI Migrations

--- HikikomoriShop.UI/Program.cs
+++ HikikomoriShop.UI/Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using HikikomoriShop.Application.Services;
+using HikikomoriShop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("HikikomoriShop.UI")
+));
+builder.Services.AddTransient<IAccountService, AccountService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = new PathString("/Account/Login");
+        options.AccessDeniedPath = new PathString("/Account/Login");
+        options.LogoutPath = "/Home/Index";
+    });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -18,7 +38,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCors(options => options.AllowAnyOrigin());
 
 app.MapControllerRoute(
     name: "default",
